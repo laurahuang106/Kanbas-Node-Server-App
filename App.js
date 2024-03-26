104d4231757c6fac69fd146a242097cb870006a6
laurahuang106 <huangyuying2018@hotmail.com>
2024-03-26 14:29:35 -0700

    lab5 3.4.4

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import Lab5 from "./Lab5.js";
 import Hello from "./Hello.js"
+import cors from "cors";
 const app = express()
+app.use(cors());
 Hello(app)
 Lab5(app);
 app.listen(4000)
