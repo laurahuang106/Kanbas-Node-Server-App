e3bd99c8a450ec2040db8e96a167ee525fa6fca9
laurahuang106 <huangyuying2018@hotmail.com>
2024-03-28 19:34:00 -0700

    lab5 4.2

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import Lab5 from "./Lab5.js";
 import Hello from "./Hello.js"
+import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 const app = express()
 app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
 Hello(app)
 Lab5(app);
 app.listen(4000)
