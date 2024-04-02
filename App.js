0a6f636627b7c95d3ba81a2a8c7befefa9291fa9
laurahuang106 <huangyuying2018@hotmail.com>
2024-04-01 20:34:23 -0700

    lab6 3.5.2

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import UserRoutes from './Users/routes.js';
 import Lab5 from "./Lab5.js";
 import Hello from "./Hello.js"
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors());
 app.use(express.json());
@@ -13,5 +17,6 @@ ModuleRoutes(app);
 AssignmentRoutes(app);
 Hello(app)
 Lab5(app);
+UserRoutes(app)
 app.listen(process.env.PORT || 4000);
 
