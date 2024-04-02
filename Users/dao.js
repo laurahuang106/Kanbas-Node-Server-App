869681608a8a501f37361f8f62bfdc372093e99d
laurahuang106 <huangyuying2018@hotmail.com>
2024-04-02 15:37:56 -0700

    lab6 3.5

--- Users/dao.js
+++ Users/dao.js
@@ -1,7 +1,11 @@
 import model from "./model.js";
-export const createUser = (user) => model.create(user);
+export const createUser = (user) => {
+    delete user._id // remove _id field just in case client sends it
+    return model.create(user);  // database will create _id for us instead    
+  }  
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
+export const findUsersByRole = (role) => model.find({ role: role });
 export const findUserByUsername = (username) =>  model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
 export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
