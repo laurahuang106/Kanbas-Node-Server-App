2ed7526c4310e78ef9a9bf632d8b25be72f67fc7
laurahuang106 <huangyuying2018@hotmail.com>
2024-04-02 14:29:48 -0700

    add users files and connect to mongoose

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
 
-// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors());
 app.use(express.json());
@@ -17,6 +17,6 @@ ModuleRoutes(app);
 AssignmentRoutes(app);
 Hello(app)
 Lab5(app);
-// UserRoutes(app)
+UserRoutes(app)
 app.listen(process.env.PORT || 4000);
 
